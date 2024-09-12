65121246cb7e6bfe50e78d18d4a9e28ae3a1d9b3
Gurpreet Deol <deol.gs@hotmail.com>
2024-09-12 07:33:05 -0700

    DB Part - II done. Migration pending.

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-app = FastAPI(name="FastAPI AWS EKS and K8s", version="1.0.0", description="FastAPI AWS EKS and K8s", debug=True)
+app = FastAPI(
+    name="FastAPI AWS EKS and K8s",
+    version="1.0.0",
+    description="FastAPI AWS EKS and K8s",
+    debug=True,
+    docs_url=None)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=HTMLResponse)
+# https://github.com/mukulmantosh/FastAPI_EKS_Kubernetes
+
+@app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/hello/{name}")
+async def hello(name: str):
+    return {"message": name}
 
 
 @app.post("/about", response_class=HTMLResponse)
@@ -21,4 +33,3 @@ async def about(request: Request):
 @app.get("/contact", response_class=HTMLResponse)
 async def contact(request: Request):
     return templates.TemplateResponse("contact.html", {"request": request})
-
